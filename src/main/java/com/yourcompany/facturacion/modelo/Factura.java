488f252a9773b24240afa86c36a4a6d7e0906a14
Anahi Quezada <aniequezada606@gmail.com>
2025-10-27 16:09:41 -0500

    Time to market tercera parte

--- src/main/java/com/yourcompany/facturacion/modelo/Factura.java
+++ src/main/java/com/yourcompany/facturacion/modelo/Factura.java
@@ -1,59 +1,25 @@
 package com.yourcompany.facturacion.modelo;
 
-import java.time.*;
 import java.util.*;
 
 import javax.persistence.*;
-import javax.persistence.Entity;
 
-import org.hibernate.annotations.*;
 import org.openxava.annotations.*;
-import org.openxava.calculators.*;
-
-import com.tuempresa.facturacion.calculadores.*;
 
 import lombok.*;
  
 @Entity @Getter @Setter
-@View(members= // Esta vista no tiene nombre, por tanto será la vista usada por defecto
-"anyo, numero, fecha;" + // Separados por coma significa en la misma línea
-"cliente;" + // Punto y coma significa nueva línea
-"detalles;" +
-"observaciones"
+@View( extendsView="super.DEFAULT", // La vista por defecto
+members="pedidos { pedidos } "
 )
-public class Factura {
-
-    @Id
-    @GeneratedValue(generator="system-uuid")
-    @Hidden
-    @GenericGenerator(name="system-uuid", strategy="uuid")
-    @Column(length=32)
-    String oid;
- 
-    @Column(length=4)
-    @DefaultValueCalculator(CurrentYearCalculator.class) // Año actual
-    int anyo;
- 
-    @Column(length=6)
-    @DefaultValueCalculator(value=CalculadorSiguienteNumeroParaAnyo.class,
-        properties=@PropertyValue(name="anyo") // Para inyectar el valor de anyo de Factura
-                                               // en el calculador antes de llamar a calculate()
-    )
-    int numero;
- 
-    @Required
-    @DefaultValueCalculator(CurrentLocalDateCalculator.class) // Fecha actual
-    LocalDate fecha;
- 
-    @TextArea
-    String observaciones;
-    
-    @ManyToOne(fetch=FetchType.LAZY, optional=false)
-    @ReferenceView("Simple") // La vista llamada 'Simple' se usará para visualizar esta referencia
-    Cliente cliente;
-    
-    @ElementCollection
-    @ListProperties("producto.numero, producto.descripcion, cantidad")
-    Collection<Detalle> detalles;
- 
+@View( name="SinClienteNiPedidos", // Una vista llamada SinClienteNiPedidos
+members=                       // que no incluye cliente ni pedidos
+    "anyo, numero, fecha;" +   // Ideal para usarse desde Pedido
+    "detalles;" +
+    "observaciones"
+)
+public class Factura extends DocumentoComercial{
+	@OneToMany(mappedBy="factura")
+    @CollectionView("SinClienteNiFactura") // Esta vista se usa para visualizar pedidos
+    private Collection<Pedido> pedidos;
 }
